f23b01d552cf581a36f85376e8b943ce3e4e517b
jubewe <ju.be.we@posteo.de>
2022-09-24 08:28:47 +0200

    3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const views = document.getElementById("views");
 const total = document.getElementById("total");
 const partners = document.getElementById("partners");
 const modlookup = document.getElementById("modlookup");
-const modChannels = document.getElementById("modChannels");
+const modchannels = document.getElementById("modchannels");
 
 function format(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -15,16 +15,42 @@ fetch("https://modlookup.3v.fi/api/user-totals/lsco").then(response => response.
     modlookup.innerHTML = 
     `
     <div class="modlookup">
-        <table>
-            <tr>
-                <td>Followers total: <a>${format(data.follows)}</a></td>
-                <td>Views total: <a>${format(data.views)}</a></td>
-            </tr>
-            <tr>
-                <td>Channels: <a>${format(data.total)}</a></td>
-                <td>Partners: <a>${format(data.partners)}</a></td>
-            </tr>
+        <center>
+        <table style="text-align: left">
+        <thead>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="td">Channels:</td>
+            <td><a>${format(data.total)}</a></td>
+        </tr>
+        <tr>
+            <td class="td">Partners:</td>
+            <td><a>${format(data.partners)}</a></td>
+        </tr>
+        <tr>
+            <td class="td">Followers total:</td>
+            <td><a>${format(data.follows)}</a></td>
+        </tr>
+        <tr>
+            <td class="td">Views total:</td>
+            <td><a>${format(data.views)}</a></td>
+        </tr>
+        </tbody>
         </table>
+        </center>
+    </div>
+    `;
+}).catch(e => {
+    console.error(e);
+    modlookup.innerHTML = 
+    `
+    <div class="modlookup">
+        <center>
+            <text>Could not load modlookup</text>
+            <br>
+            <text>(${e.message})</text>
+        </center>
     </div>
     `;
 });
@@ -38,6 +64,7 @@ fetch("https://modlookup.3v.fi/api/user-v3/lsco").then(response => response.json
         }
     }
     fetch(`https://api.ivr.fi/v2/twitch/user${channelsList}`).then(response => response.json()).then(request => {
+        let statchannels = "";
         let data = request.sort((x, y) => Number(y.followers) - Number(x.followers));
         for (const channel of data) {
             if (channel.login && channel.login !== "lsco") {
@@ -48,38 +75,84 @@ fetch("https://modlookup.3v.fi/api/user-v3/lsco").then(response => response.json
                     `
                         <br>
                         <div class="game">
-                        <text>LIVE:</text>
-                        <a href="https://www.twitch.tv/directory/game/${encodeURI(channel.stream.game.displayName)}/" target="_blank" rel="noopener noreferrer">${channel.stream.game.displayName}</a>
-                        <text>~</text>
-                        <text style="color: ${channel.chatColor ? channel.chatColor : "#123456"};">
-                        ${format(channel.stream.viewersCount)}
-                        </text>
-                        <text>~</text>
-                        <text>
-                        ${channel.stream.title}
-                        </text>
+                            <center>
+                            <text>LIVE:</text>
+                                <a href="https://www.twitch.tv/directory/game/${encodeURI(channel.stream.game.displayName).toString()}/" target="_blank" rel="noopener noreferrer">${channel.stream.game.displayName}</a>
+                                <text>~</text>
+                                <text style="color: ${channel.chatColor ? channel.chatColor : "#123456"};">
+                                ${format(channel.stream.viewersCount)}
+                                </text>
+                                <br>
+                                <text>
+                                ${channel.stream.title}
+                                </text>
+                            </center>
                         </div>
                     `;
                 }
                 if (channel.roles.isPartner) {
                     badges +=
                     `
-                        <img class="badge" src="https://static-cdn.jtvnw.net/badges/v1/d12a2e27-16f6-41d0-ab77-b780518f00a3/3">
+                        <img title="Partner" class="badge" src="https://static-cdn.jtvnw.net/badges/v1/d12a2e27-16f6-41d0-ab77-b780518f00a3/3">
                     `
                 }
-                modChannels.innerHTML += 
+                let bgcolor = undefined;
+
+                if(islive !== ""){
+                    bgcolor = "#6441a5";
+                }
+                // modchannels.innerHTML += 
+                statchannels += 
                 `
-                    <div class="line">
-                        <img class="logo" src="${channel.logo}">
-                        ${badges}
-                        <a style="color: ${channel.chatColor ? channel.chatColor : "#123456"};" href="https://www.twitch.tv/${encodeURIComponent(channel.login)}/" target="_blank" rel="noopener noreferrer">
-                        ${channel.displayName ? channel.displayName : channel.login}
-                        </a>
-                        <i class="followers"> ~${format(channel.followers)}</i>
-                        ${islive}
+                    <div class="line_2" ${bgcolor ? `style="background-color: ${bgcolor}"` : ''}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td class="td_1">
+                                        <div class="spacer_"></div>
+                                        <a href=${channel.logo} target="_blank" rel="noopener noreferrer"><img class="logo" src="${channel.logo}"></a>
+                                        <div class="spacer_"></div>
+                                    </td>
+                                    <td class="td_2">
+                                        <center>
+                                        <a style="color: ${channel.chatColor ? channel.chatColor : "#123456"};" href="https://www.twitch.tv/${encodeURIComponent(channel.login)}/" target="_blank" rel="noopener noreferrer">${channel.displayName ? channel.displayName : channel.login}</a>
+                                        <div class="spacer_"></div>
+                                        ${badges}
+                                        ${(badges !== "" ? `<div class="spacer_"></div>` : ``)}
+                                        </center>
+                                    </td>
+                                    <td class="td_3">
+                                        <text style="float:right">~${format(channel.followers)}</text>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        ${(islive !== "" ? `<div>${islive}</div>` : ``)}
                     </div>
                 `;
             }
         }
+
+        modchannels.innerHTML = 
+        `
+            <div class="modchannels">
+                <center>
+                    ${statchannels}
+                </center>
+            </div>
+        `;
     })
+})
+.catch(e => {
+    console.error(e);
+    modchannels.innerHTML = 
+    `
+        <div class="modchannels">
+            <center>
+                <text>Error: Could not load stats </text>
+                <br>
+                <text>(${e.message})</text>
+            </center>
+        </div>
+    `;
 });
