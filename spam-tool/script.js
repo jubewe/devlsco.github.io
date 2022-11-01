a1ab9698114f2ba22ebfca014dacb5f522fe69ca
LsCo <lennoxoo2007@gmail.com>
2022-11-01 17:22:13 +0100

    .
    .

--- spam-tool/script.js
+++ spam-tool/script.js
@@ -53,27 +53,27 @@ async function send(version) {
         cooldown.delete("");
     }, 2500);
 
-    const textRegEx = new RegExp("^.{1,450}$", "");
-    const numberRegEx = new RegExp("^[0-9]+$", "");
-    const channelRegEx = new RegExp("^\\w{1,25}$", "");
-
+    
     if (await readCokie("token") === null || await readCokie("login") === null || await readCokie("clientid") === null) {
         const url = "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=oja440lzo9ek9ogxnymgtoq3j4m4yq&redirect_uri=https://devlsco.github.io/spam-tool&scope=chat:read+chat:edit+channel:moderate";
-        return response(`Token or clientID not found. (Click on <a href="${url}">Login</a> to request a new Token)`);
+        return await response(`Token or clientID not found. (Click on <a href="${url}">Login</a> to request a new Token)`);
     }
-
+    
     console.log(channel.value);
+    const channelRegEx = new RegExp("^\\w{1,25}$", "");
     if (!channelRegEx.test(String(channel.value))) {
-        return response(`Error: Channel must match pattern "${channelRegEx}".`);
+        return await response(`Error: Channel must match pattern "${channelRegEx}".`);
     }
+    const textRegEx = new RegExp("^.{1,450}$", "");
     if (!textRegEx.test(String(text.value))) {
-        return response(`Error: Text must match pattern "${textRegEx}".`);
+        return await response(`Error: Text must match pattern "${textRegEx}".`);
     }
+    const numberRegEx = new RegExp("^[0-9]+$", "");
     if (!numberRegEx.test(Number(number.value))) {
-        return response(`Error: Number must match pattern "${numberRegEx}".`);
+        return await response(`Error: Number must match pattern "${numberRegEx}".`);
     }
     if (Number(number.value > 25)) {
-        return response(`Error: Number maximum. 25`);
+        return await response(`Error: Number maximum. 25`);
     }
 
     await fetch(`https://api.twitch.tv/helix/users?login=${channel.value}`, {
