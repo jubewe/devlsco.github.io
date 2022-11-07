65261675b3e5d3d6b69e02682004aa275ed68a6c
LsCo <lennoxoo2007@gmail.com>
2022-11-07 15:43:08 +0100

    .
    .

--- pyramid-tool/script.js
+++ pyramid-tool/script.js
@@ -1,39 +1,42 @@
-const channel = document.getElementById("channel");
-const size = document.getElementById("size");
-const text = document.getElementById("text");
-
 const cooldown = new Map();
 
-function response(text) {
-    const response_ = document.getElementById("response");
-    if (!text) return `Error: Function response is " ${text} "`;
-    return response_.innerHTML = text;
-}
+document.getElementById("button").addEventListener("click", run);
 
-if (window.location.hash.includes("#access_token=")) {
-    fetch(`https://id.twitch.tv/oauth2/validate`, {
-        headers: {
-            "Authorization": `Bearer ${window.location.hash.split("#access_token=")[1].split("&")[0]}`,
-        }
-    }).then(response => response.json()).then(async (data) => {
-        await setCookie("token", window.location.hash.split("#access_token=")[1].split("&")[0]);
-        await setCookie("login", data.login);
-        await setCookie("clientid", data.client_id).then(() => {
-            const url = document.baseURI.split("#access_token=")
-            window.location.replace(url[0]);
+const url = "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=oja440lzo9ek9ogxnymgtoq3j4m4yq&redirect_uri=https://devlsco.github.io/pyramid-tool&scope=chat:read+chat:edit+channel:moderate";
+
+if (window.location.hash.includes("#access_token")) {
+    (async () => {
+        await fetch("https://id.twitch.tv/oauth2/validate", {
+            headers: {
+                "Authorization": `Bearer ${window.location.hash.split("#access_token=")[1].split("&")[0]}`
+            }
+        }).then(response => response.json()).then(async (data) => {
+            await setCookie("token", window.location.hash.split("#access_token=")[1].split("&")[0], 365);
+            await setCookie("login", data.login, 365);
+            await setCookie("clientid", data.client_id, 365).then(async () => {
+                const url = document.baseURI.split("#access_token=");
+                await window.location.replace(url[0]);
+            })
         })
-    })
+    })();
 }
 
-async function readCokie(name) {
+if (!readCokie("token") || !readCokie("clientid") || !readCokie("login")) response(`Click on <a href="${url}">Login</a> to request a Token`);
+
+function response(message) {
+    const res = document.getElementById("response");
+    return res.innerHTML = message;
+}
+
+function readCokie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return await c.substring(nameEQ.length, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
-    return await null;
+    return null;
 }
 
 async function setCookie(name, value, days) {
@@ -50,73 +53,80 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function send(version) {
-    if (cooldown.has("")) return;
-    cooldown.set("", Date.now());
-    setTimeout(() => {
-        cooldown.delete("");
-    }, 5000);
+async function run() {
+    const channel = document.getElementById("channel").value;
+    const message = document.getElementById("message").value;
+    const size = document.getElementById("size").value;
 
+    if (await !readCokie("token") || await !readCokie("clientid") || await !readCokie("login")) return;
 
-    if (await readCokie("token") === null || await readCokie("login") === null || await readCokie("clientid") === null) {
-        const url = "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=oja440lzo9ek9ogxnymgtoq3j4m4yq&redirect_uri=https://devlsco.github.io/pyramid-tool&scope=chat:read+chat:edit+channel:moderate";
-        return await response(`Token or clientID not found. (Click on <a href="${url}">Login</a> to request a new Token)`);
-    }
+    if (cooldown.has("lol")) return;
+    cooldown.set("lol", Date.now());
+    setTimeout(async () => {
+        await cooldown.delete("lol");
+    }, 2500);
 
-    await fetch(`https://id.twitch.tv/oauth2/validate`, {
-        headers: {
-            "Authorization": `Bearer ${await readCokie("token")}`,
-        }
-    }).then(response => response.json()).then(async (dataaaaa) => {
-        const url = "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=oja440lzo9ek9ogxnymgtoq3j4m4yq&redirect_uri=https://devlsco.github.io/pyramid-tool&scope=chat:read+chat:edit+channel:moderate";
-        if (dataaaaa.status !== undefined) return await response(`Error: ${dataaaaa.message} | Status: ${dataaaaa.status} | (Click on <a href="${url}">Login</a> to request a new Token)`);
-
-        const channelRegEx = new RegExp("^\\w{1,25}$", "");
-        if (!channelRegEx.test(String(channel.value))) {
-            return await response(`Error: Channel must match pattern "${channelRegEx}".`);
-        }
-        const textRegEx = new RegExp("^.{1,35}$", "");
-        if (!textRegEx.test(String(text.value))) {
-            return await response(`Error: Text must match pattern "${textRegEx}".`);
-        }
-        const sizeRegEx = new RegExp("^[0-9]+$", "");
-        if (!sizeRegEx.test(Number(size.value))) {
-            return await response(`Error: Size must match pattern "${sizeRegEx}".`);
-        }
-        if (Number(size.value > 25)) {
-            return await response(`Error: Size maximum. 25`);
-        }
-
-        await fetch(`https://api.twitch.tv/helix/users?login=${channel.value}`, {
+    (async () => {
+        await fetch("https://id.twitch.tv/oauth2/validate", {
             headers: {
-                "Authorization": `Bearer ${await readCokie("token")}`,
-                "Client-Id": `${await readCokie("clientid")}`
+                "Authorization": `Bearer ${await readCokie("token")}`
             }
-        }).then(res => res.json()).then(async ({ data }) => {
-            if (data.status !== undefined) return response(`Channel was not found `);
-            if (version === 1) {
-                new WebSocket("wss://irc-ws.chat.twitch.tv/").onopen = async function () {
-                    await this.send(`PASS oauth:${await readCokie("token")}`);
-                    await this.send(`NICK ${await readCokie("login")}`);
-                    const text_ = text.value + " ";
-                    for (let i = 1; i <= size.value; i++) {
-                        await this.send(`PRIVMSG #${channel.value} :${text_.repeat(i)}`);
-                        await sleep(100)
-                    }
+        }).then(response => response.json()).then(async (data) => {
 
-                    for (let i = (size.value - 1); i > 0; i--) {
-                        await this.send(`PRIVMSG #${channel.value} :${text_.repeat(i)}`);
-                        await sleep(100)
-                    }
+            if (data.status !== undefined) return await response(`Error: ${data.message} - Status: ${data.status} - [ Click on <a href="${url}">Login</a> to request a new Token ]`);
+
+            const channelRegEx = new RegExp("^\\w{1,25}$", "");
+            if (!channelRegEx.test(String(channel))) {
+                return await response(`[ Error: Channel must match pattern "${channelRegEx}". ]`);
+            }
+            const messageRegEx = new RegExp("^.{1,450}$", "");
+            if (!messageRegEx.test(String(message))) {
+                return await response(`[ Error: Message must match pattern "${messageRegEx}". ]`);
+            }
+            const sizeRegEx = new RegExp("^[0-9]+$", "");
+            if (!sizeRegEx.test(Number(size))) {
+                return await response(`[ Error: Size must match pattern "${sizeRegEx}". ]`);
+            }
+            if (Number(size > 25)) {
+                return await response(`[ Error: Size maximum. 25 ]`);
+            }
+
+            await fetch(`https://api.twitch.tv/helix/users?login=${channel}`, {
+                headers: {
+                    "Authorization": `Bearer ${readCokie("token")}`,
+                    "Client-Id": `${readCokie("clientid")}`
+                }
+            }).then(res => res.json()).then(async ({ data }) => {
+
+                if (data.status !== undefined) return response(`Channel was not found `);
+
+                new WebSocket("wss://irc-ws.chat.twitch.tv/").onopen = async function () {
+                    await this.send(`PASS oauth:${readCokie("token")}`);
+                    await this.send(`NICK ${readCokie("login")}`);
+                    (async () => {
+                        for (let i = 1; i <= size; i++) {
+                            const message_ = message.+ " ";
+                            await this.send(`PRIVMSG #${channel} :${message_.repeat(i)}`);
+                            await sleep(100)
+                        }
+    
+                        for (let i = (size - 1); i > 0; i--) {
+                            await this.send(`PRIVMSG #${channel} :${message_.repeat(i)}`);
+                            await sleep(100)
+                        }
+                    })();
+
                     setTimeout(() => {
                         this.close();
                     }, 5000);
                 }
-            }
-            return response(`Successfully sent ${Number(size.value)} size length pyramid in ${String(channel.value)}`);
-        }).catch(error => {
-            console.log(error);
-            return response(`Error: ${error.message}`)
-        });
-    });
+
+                return response(`Successfully sent a ${Number(size)} size length Pyramid in #${String(channel)}`);
+
+            }).catch(error => {
+                console.log(error);
+                return response(`Error: ${error.message}`)
+            });
+        })
+    })();
 }
